3c181a61cef2e1c6f5bc98a5042db7f805c70b32
Vins4652 <vinaymannur9@gmail.com>
2023-04-04 12:51:05 +0530

    vinay commit

--- src/main/java/projrctName/Test4.java
+++
@@ -1,8 +0,0 @@
-package projrctName;
-
-public class Test4 {
-
-	public static void main(String[] args) {
-		System.out.println("-----Sample Test-----");
-	}
-}
---
+++ src/main/java/projrctName/Test7.java
@@ -0,0 +1,8 @@
+package projrctName;
+
+public class Test7 {
+
+	public void vinay() {
+		System.out.println("------merger-------");
+	}
+}
